5b920ff4d4c9adf6363f1583b251822a52695385
ShowkatJamil <showkatjamil2012@gmail.com>
2023-08-28 19:04:26 +0600

    final commit with all the functionalities

---
+++ android/app/src/main/kotlin/com/example/tasklist_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tasklist_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:tasklist_ui/scenes/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
